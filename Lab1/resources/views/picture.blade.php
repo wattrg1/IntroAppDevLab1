8e841332a367112dec244c7f6c7b93cd44853914
Reuben Watt (1000063488) <WATTRG1@student.op.ac.nz>
2020-07-24 14:19:27 +1200

    Timetable page is accurate

---
+++ Lab1/resources/views/picture.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Bootstrap 4, from LayoutIt!</title>
+
+    <meta name="description" content="Source code generated using layoutit.com">
+    <meta name="author" content="LayoutIt!">
+
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+
+  </head>
+  <body>
+
+    <div class="container-fluid">
+	<div class="row">
+		<div class="col-md-12">
+			<h3 class="text-center">
+				h3. Lorem ipsum dolor sit amet.
+			</h3>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-12">
+			<img alt="Bootstrap Image Preview" src="https://www.layoutit.com/img/sports-q-c-140-140-3.jpg">
+		</div>
+	</div>
+</div>
+
+    <script src="js/jquery.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+    <script src="js/scripts.js"></script>
+  </body>
+</html>
---
+++ Lab1/resources/views/timetable.blade.php
@@ -0,0 +1,355 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Bootstrap 4, from LayoutIt!</title>
+
+    <meta name="description" content="Source code generated using layoutit.com">
+    <meta name="author" content="LayoutIt!">
+
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+
+  </head>
+  <body>
+
+    <div class="container-fluid">
+	<div class="row">
+		<div class="col-md-12">
+			<h1 class="text-center">
+				Timetable
+			</h1>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-12">
+			<table class="table">
+				<thead>
+					<tr>
+						<th>
+							Time
+						</th>
+						<th>
+							Monday
+						</th>
+						<th>
+							Tuesday
+						</th>
+						<th>
+							Wednesday
+                        </th>
+                        <th>
+							Thursday
+                        </th>
+                        <th>
+							Friday
+                        </th>
+                        <th>
+							Saturday
+                        </th>
+                        <th>
+							Sunday
+						</th>
+					</tr>
+				</thead>
+				<tbody>
+                    <tr>
+						<td>
+							7:00
+						</td>
+						<td>
+							
+						</td>
+						<td>
+							
+						</td>
+						<td>
+							
+                        </td>
+                        <td>
+                            
+                        </td>
+                        <td>
+							
+                        </td>
+                        <td>
+							
+                        </td>
+                        <td>
+							
+						</td>
+					</tr>
+					<tr>
+						<td>
+							8:00
+						</td>
+						<td class="table-active">
+							IN602 Software Engineering
+						</td>
+						<td class="table-success">
+							IN616 Operating System Concepts
+						</td>
+						<td>
+							
+                        </td>
+                        <td class="table-active">
+                            IN602 Software Engineering
+                        </td>
+                        <td>
+							
+                        </td>
+                        <td>
+							
+                        </td>
+                        <td>
+							
+						</td>
+                    </tr>
+                    <tr>
+						<td>
+							9:00
+						</td>
+						<td class="table-active">
+							
+						</td>
+						<td class="table-success">
+							
+						</td>
+						<td>
+							
+                        </td>
+                        <td class="table-active">
+                            
+                        </td>
+                        <td>
+							
+                        </td>
+                        <td>
+							
+                        </td>
+                        <td>
+							
+						</td>
+                    </tr>
+                    <tr>
+						<td>
+							10:00
+						</td>
+						<td>
+							
+						</td>
+						<td>
+							
+						</td>
+						<td>
+							
+                        </td>
+                        <td>
+                            
+                        </td>
+                        <td>
+							
+                        </td>
+                        <td>
+							
+                        </td>
+                        <td>
+							
+						</td>
+					</tr>
+					<tr>
+						<td>
+							11:00
+						</td>
+						<td>
+							
+						</td>
+						<td>
+							
+						</td>
+						<td>
+							
+                        </td>
+                        <td>
+                            
+                        </td>
+                        <td>
+							
+                        </td>
+                        <td>
+							
+                        </td>
+                        <td>
+							
+						</td>
+					</tr>
+					<tr>
+						<td>
+							12:00
+						</td>
+						<td>
+							
+						</td>
+						<td>
+							
+						</td>
+						<td>
+							
+                        </td>
+                        <td>
+                            
+                        </td>
+                        <td>
+							
+                        </td>
+                        <td>
+							
+                        </td>
+                        <td>
+							
+						</td>
+                    </tr>
+                    <tr>
+						<td>
+							1:00
+						</td>
+						<td>
+							
+						</td>
+						<td class="table-warning">
+							IN605 Databases 2
+						</td>
+						<td>
+							
+                        </td>
+                        <td class="table-warning">
+							IN605 Databases 2
+						</td>
+                        <td class="table-danger">
+							IN607 Intro to App Dev
+                        </td>
+                        <td>
+							
+                        </td>
+                        <td>
+							
+						</td>
+					</tr>
+					<tr>
+						<td>
+							2:00
+						</td>
+						<td>
+							
+						</td>
+						<td class="table-warning">
+							
+						</td>
+						<td>
+							
+                        </td>
+                        <td class="table-warning">
+							
+						</td>
+                        <td class="table-danger">
+							
+                        </td>
+                        <td>
+							
+                        </td>
+                        <td>
+							
+						</td>
+					</tr>
+					<tr>
+						<td>
+							3:00
+						</td>
+						<td>
+							
+						</td>
+						<td class="table-danger">
+							IN607 Intro to App Dev
+                        </td>
+						<td>
+							
+                        </td>
+                        <td class="table-success">
+							IN616 Operating System Concepts
+						</td>
+                        <td>
+							
+                        </td>
+                        <td>
+							
+                        </td>
+                        <td>
+							
+						</td>
+                    </tr>
+                    <tr>
+						<td>
+							4:00
+						</td>
+						<td>
+							
+						</td>
+						<td class="table-danger">
+							
+                        </td>
+						<td>
+							
+                        </td>
+                        <td class="table-success">
+							
+						</td>
+                        <td>
+							
+                        </td>
+                        <td>
+							
+                        </td>
+                        <td>
+							
+						</td>
+                    </tr>
+                    <tr>
+						<td>
+							5:00
+						</td>
+						<td>
+							
+						</td>
+						<td>
+							
+						</td>
+						<td>
+							
+                        </td>
+                        <td>
+                            
+                        </td>
+                        <td>
+							
+                        </td>
+                        <td>
+							
+                        </td>
+                        <td>
+							
+						</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+	</div>
+</div>
+
+    <script src="js/jquery.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+    <script src="js/scripts.js"></script>
+  </body>
+</html>
